d437f20cdbc80edd591432bb82b5398778c0881c
olemagnus <olestovern@gmail.com>
2018-11-22 15:57:51 +0100

    adds scrollhint

---
+++ src/scrollHint.js
@@ -0,0 +1,26 @@
+function activeHeading(headings, navItems, scrollPos) {
+  // Makes the nodeList to an array of htmlElements
+  const htmlHeadings = [].slice.call(headings);
+  const prescrolledItems = htmlHeadings.filter(
+    // 20px gives us some headroom above the heading, so it always becomes active when linked to
+    h => h.offsetTop < scrollPos + 20
+  );
+
+  if (prescrolledItems.length) {
+    // Finds the index of the first item of those we have not scrolled past yet
+    toggleActiveNavClass(
+      navItems,
+      htmlHeadings.indexOf(prescrolledItems[prescrolledItems.length - 1])
+    );
+  }
+}
+
+function toggleActiveNavClass(navItems, index) {
+  // Clean up
+  Array.from(navItems).forEach(item =>
+    item.classList.remove("b-nav-list__link--active")
+  );
+  navItems[index].classList.add("b-nav-list__link--active");
+}
+
+export { activeHeading, toggleActiveNavClass };
