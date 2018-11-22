017e6f255c8daa7d1343f83ad7bb973cfdfd5612
olemagnus <olestovern@gmail.com>
2018-11-22 15:58:03 +0100

    adds main js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log("test");
+import Stickyfill from "../node_modules/stickyfilljs/dist/stickyfill";
+import { activeHeading } from "./scrollHint";
+
+// Init stickyscroll polyfill
+const elements = document.querySelectorAll(".b-nav-list--sticky");
+Stickyfill.add(elements);
+
+// Create listener on scrollHint
+// Check if we got some body text and a sticky menu
+if (
+  document.querySelector(".t-body-text") &&
+  document.querySelector(".b-nav-list--sticky ")
+) {
+  const navItems = document.querySelectorAll(
+    ".b-nav-list--sticky > .b-nav-list__items > .b-nav-list__item--anchor"
+  );
+  const headings = document.querySelectorAll(".t-body-text > h2");
+  // Only add the event listener if the number of nav list items and h2 headings match.
+  if (navItems.length === headings.length) {
+    window.addEventListener("scroll", () => {
+      const scrollPosition = window.pageYOffset;
+
+      activeHeading(headings, navItems, scrollPosition);
+    });
+  }
+}
