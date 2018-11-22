f831b724158215a8ed9ebcdb7873d16bd96c0a10
olemagnus <olestovern@gmail.com>
2018-11-22 17:31:12 +0100

    Makes first item inactive if scrolled above

--- src/scrollHint.js
+++ src/scrollHint.js
@@ -1,16 +1,23 @@
 function activeHeading(headings, navItems, scrollPos) {
+  // 20px gives us some headroom above the heading, so it always becomes active when linked to
+  const headingSpace = 20;
+
   // Makes the nodeList to an array of htmlElements
   const htmlHeadings = [].slice.call(headings);
-  const prescrolledItems = htmlHeadings.filter(
-    // 20px gives us some headroom above the heading, so it always becomes active when linked to
-    h => h.offsetTop < scrollPos + 20
+  const scrolledPastItems = htmlHeadings.filter(
+    h => h.offsetTop < scrollPos + headingSpace
   );
+  // Removes the active classes if we have not scrolled to a heading yet.
+  if (!scrolledPastItems.length) {
+    toggleActiveNavClass(navItems);
+    return;
+  }
 
-  if (prescrolledItems.length) {
+  if (scrolledPastItems.length) {
     // Finds the last item we scrolled past
     toggleActiveNavClass(
       navItems,
-      htmlHeadings.indexOf(prescrolledItems[prescrolledItems.length - 1])
+      htmlHeadings.indexOf(scrolledPastItems[scrolledPastItems.length - 1])
     );
   }
 }
@@ -20,7 +27,9 @@ function toggleActiveNavClass(navItems, index) {
   Array.from(navItems).forEach(item =>
     item.classList.remove("b-nav-list__link--active")
   );
-  navItems[index].classList.add("b-nav-list__link--active");
+  // Only add the class if we have an index
+  index !== undefined &&
+    navItems[index].classList.add("b-nav-list__link--active");
 }
 
 export { activeHeading, toggleActiveNavClass };
