ee407f59b63aa7a19ad7e1503e252c3f6111c9c8
olemagnus <olestovern@gmail.com>
2019-01-25 10:44:05 +0100

    Adds zenscroll to collapsible

--- src/collapsible.js
+++ src/collapsible.js
@@ -1,19 +1,32 @@
+import zenscroll from "zenscroll";
+
 function collapsible(e) {
+  const element = e.currentTarget;
   const expanded =
-    e.currentTarget.getAttribute("aria-expanded") === "true" ? true : false;
+    element.getAttribute("aria-expanded") === "true" ? true : false;
   // Toggle expanded value
-  e.currentTarget.setAttribute("aria-expanded", String(!expanded));
+  element.setAttribute("aria-expanded", String(!expanded));
 
   // Toggle active modifier class
-  const elemClasses = e.currentTarget.classList;
+  const elemClasses = element.classList;
   if (elemClasses.contains("b-collapsible__button--active")) {
     elemClasses.remove("b-collapsible__button--active");
   } else {
     elemClasses.add("b-collapsible__button--active");
   }
 
-  const ariaControls = e.currentTarget.getAttribute("aria-controls");
+  const ariaControls = element.getAttribute("aria-controls");
   toggleContent(ariaControls, expanded);
+}
+
+function findWrapper(child) {
+  // If the parent is the component wrapper, then return it, so we can scroll it into view later.
+  // If it is a child, then just return itself.
+  if (child.parentElement.getAttribute("class") === "b-collapsible") {
+    return child.parentElement;
+  } else {
+    return child;
+  }
 }
 
 function toggleContent(ariaControls, expanded) {
@@ -23,6 +36,8 @@ function toggleContent(ariaControls, expanded) {
     : content.removeAttribute("hidden");
 
   content.setAttribute("aria-hidden", String(expanded));
+  // Animates the scroll to the element, making sure the top of the expanding area is in the window view
+  zenscroll.intoView(findWrapper(content), 300);
 }
 
 export { collapsible };
