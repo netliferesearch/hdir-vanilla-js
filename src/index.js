3f901b92c54c7c7c56e599dc7a4d2496cfa10203
olemagnus <olestovern@gmail.com>
2019-02-13 13:45:35 +0100

    Removes scroll hint

--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
 import "@babel/polyfill";
 import "url-polyfill";
-import Stickyfill from "../node_modules/stickyfilljs/dist/stickyfill";
-import debounce from "lodash.debounce";
-import { activeHeading } from "./scrollHint";
 import { collapsible, collapseFromUrl } from "./collapsible";
 import { setParams, setInputValue, resetSearchParams } from "./filterToggler";
 import { makeListExpandable } from "./expandableList";
-
-// Init stickyscroll polyfill
-const stickyElement = document.querySelectorAll(".b-nav-list--sticky");
-Stickyfill.add(stickyElement);
-
-// Create listener on scrollHint
-// Check if we got some body text and a sticky menu
-if (
-  document.querySelector(".t-body-text") &&
-  document.querySelector(".b-nav-list--sticky ")
-) {
-  const navItems = document.querySelectorAll(
-    ".b-nav-list--sticky > .b-nav-list__items > .b-nav-list__item--anchor"
-  );
-  const headings = document.querySelectorAll(".t-body-text > h2");
-  // Only add the event listener if the number of nav list items and h2 headings match.
-  if (navItems.length === headings.length) {
-    window.addEventListener(
-      "scroll",
-      debounce(() => {
-        activeHeading(headings, navItems, window.pageYOffset);
-      }, 16.66) // 16.66ms is 60fps
-    );
-  }
-}
 
 // Collapsible
 [...document.querySelectorAll(".b-collapsible__button")].forEach(item => {
