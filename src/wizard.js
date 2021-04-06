341355295e9d5c961cce9b5c2bcdbc94464b3f42
thomaslgj <thomaslgj@gmail.com>
2021-04-06 10:50:02 +0200

    Update wizard code, add radio buttons option

--- src/wizard.js
+++ src/wizard.js
@@ -1,62 +1,83 @@
 import zenscroll from "zenscroll";
 export const wizard = (steps) => {
 
+  const openNext = (step) => {
+    const nextStep = steps[step.dataset.step];
+    const nextStepCollapsible = nextStep.querySelector('.b-collapsible');
+    const nextStepTrigger = nextStep.querySelector('button');
+    if (!nextStepCollapsible.classList.contains('b-collapsible--active')) {
+      nextStepTrigger.click();
+    }
+  };
+
+  const showResults = (step) => {
+    const nextStep = steps[step.dataset.step];
+    const submit = step.querySelector('button[data-submit]');
+    nextStep.classList.remove('hide');
+    setTimeout(() => {
+      zenscroll.to(nextStep);
+      if (submit) {
+        submit.innerHTML = "Oppdater resultat";
+      }
+    }, 100);
+  };
+
   steps.forEach(step => {
     const totalSteps = steps.length;
     const stepType = step.dataset.stepType;
     const inputType = step.dataset.inputType;
 
-    if (inputType === 'select') {
+    if (inputType === 'dropValue') {
       const input = step.querySelector('select');
 
       input.addEventListener("change", function (e) {
 
         // More steps to come, open next
         if (step.dataset.step < totalSteps - 1) {
-          const nextStep = steps[step.dataset.step];
-          const nextStepCollapsible = nextStep.querySelector('.b-collapsible');
-          const nextStepTrigger = nextStep.querySelector('button');
-          if (!nextStepCollapsible.classList.contains('b-collapsible--active')) {
-            nextStepTrigger.click();
-          }
+          openNext(step);
         }
 
         // The last step before results, show results
         if (step.dataset.step == (totalSteps - 1)) {
-          const nextStep = steps[step.dataset.step];
-          nextStep.classList.remove('hide');
-          setTimeout(() => {
-            zenscroll.to(nextStep);
-            submit.innerHTML = "Oppdater resultat";
-          }, 100);
+          showResults(step);
         }
       });
     }
 
-    if (inputType === 'checkboxes') {
+    if (inputType === 'checkValue') {
       const submit = step.querySelector('button[data-submit]');
       
-      submit.addEventListener("click", function (e) {
+      submit && submit.addEventListener("click", function (e) {
 
         // More steps to come, open next
         if (step.dataset.step < totalSteps - 1) {
-          const nextStep = steps[step.dataset.step];
-          const nextStepCollapsible = nextStep.querySelector('.b-collapsible');
-          const nextStepTrigger = nextStep.querySelector('button');
-          if (!nextStepCollapsible.classList.contains('b-collapsible--active')) {
-            nextStepTrigger.click();
-          }
+          openNext(step);
         }
 
         // The last step before results, show results
         if (step.dataset.step == (totalSteps - 1)) {
-          const nextStep = steps[step.dataset.step];
-          nextStep.classList.remove('hide');
-          setTimeout(() => {
-            zenscroll.to(nextStep);
-            submit.innerHTML = "Oppdater resultat";
-          }, 100);
+          showResults(step);
         }
+      });
+
+    }
+
+    if (inputType === 'radioValue') {
+      const radios = step.querySelectorAll('input[type="radio"]');
+
+      radios.forEach(input => {
+        input.addEventListener("change", function (e) {
+
+          // More steps to come, open next
+          if (step.dataset.step < totalSteps - 1) {
+            openNext(step);
+          }
+
+          // The last step before results, show results
+          if (step.dataset.step == (totalSteps - 1)) {
+            showResults(step);
+          }
+        });
       });
 
     }
